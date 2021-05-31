26654b6896975b9287e4191bbbcd78fd2a0d1e2b
WhoSoup <who.soup@gmail.com>
2021-05-31 14:29:21 +0200

    ts base

---
+++ test/index.ts
@@ -0,0 +1,100 @@
+import {
+    Token,
+    TOKEN_PROGRAM_ID,
+    MintLayout,
+    ASSOCIATED_TOKEN_PROGRAM_ID
+} from '@solana/spl-token';
+import {
+    BPF_LOADER_PROGRAM_ID,
+    BpfLoader,
+    Account,
+    Keypair,
+    LAMPORTS_PER_SOL,
+    Signer,
+    Transaction,
+    TransactionInstruction,
+    AccountMeta,
+    PublicKey,
+    SYSVAR_RENT_PUBKEY,
+    sendAndConfirmTransaction,
+    SystemProgram
+} from '@solana/web3.js';
+import { Connection } from '@solana/web3.js';
+import { BADNAME } from 'dns';
+import * as fs from 'fs';
+
+const connection = new Connection('http://localhost:8899');
+const funder = new Keypair();
+
+const token_id = new Keypair();
+const fee_authority = new Keypair();
+
+const mint_authority = new Keypair();
+const deploy_key = new Keypair();
+const programId = deploy_key.publicKey;
+
+const token = new Token(
+    connection,
+    token_id.publicKey,
+    TOKEN_PROGRAM_ID,
+    new Account(funder.secretKey)
+);
+
+(async () => {
+    console.log(`Funding ${funder.publicKey.toBase58()} with 20 SOL`);
+    let sig = await connection.requestAirdrop(
+        funder.publicKey,
+        20 * LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(sig);
+
+    console.log(`Deploy BPF to ${deploy_key.publicKey.toBase58()}`);
+    const programdata = fs.readFileSync('../program/target/deploy/staking.so');
+    if (
+        !(await BpfLoader.load(
+            connection,
+            funder,
+            deploy_key,
+            programdata,
+            BPF_LOADER_PROGRAM_ID
+        ))
+    ) {
+        console.log('Loading bpf failed');
+        process.exit(1);
+    }
+
+    console.log(`Creating new SPL Token ${token_id.publicKey.toBase58()}`);
+
+    // doing it manually since the library doesn't accept pre-defined pubkeys
+    const balanceNeeded = await Token.getMinBalanceRentForExemptMint(
+        connection
+    );
+
+    const transaction = new Transaction();
+    transaction.add(
+        SystemProgram.createAccount({
+            fromPubkey: funder.publicKey,
+            newAccountPubkey: token_id.publicKey,
+            lamports: balanceNeeded,
+            space: MintLayout.span,
+            programId: TOKEN_PROGRAM_ID
+        })
+    );
+
+    transaction.add(
+        Token.createInitMintInstruction(
+            TOKEN_PROGRAM_ID,
+            token_id.publicKey,
+            0,
+            mint_authority.publicKey,
+            null
+        )
+    );
+
+    await sendAndConfirmTransaction(connection, transaction, [
+        funder,
+        token_id
+    ]);
+
+    console.log(`Attempting to initialize`);
+})();
