f4264ca0ab9c076de09c4d9f4a50b42adb6102a7
WhoSoup <who.soup@gmail.com>
2021-09-23 14:57:09 +0200

    expand script

--- test_server/scripts/issue-604.ts
+++ test_server/scripts/issue-604.ts
@@ -15,11 +15,15 @@ import {
     SystemProgram,
     Transaction
 } from '@solana/web3.js';
-import { Instruction, Staking } from '@zoints/staking';
+import { Instruction, Instructions, Staking } from '@zoints/staking';
+import BN from 'bn.js';
 import { create } from 'domain';
 
 const programId = new PublicKey('GGgStUEFvrGGj3aBH6mcLzyrMM6EyKDDuLZYbgZczq4Q');
 const mint = new PublicKey('5JKMQjbzy1T4w8e84wZ6rVmRZXi7gz4t7ugA9BrYNJuj');
+const feeRecipient = new PublicKey(
+    '274Mk1JY6sKNtbeWtZsW5DSC3SWmCknmx7qsPR1EWxpQ'
+);
 const mintAuthority = Keypair.fromSecretKey(
     Buffer.from(
         '2c6433c70d57bc3cf4e124f41974e91e302271970fcccb0a0c9eea790cad255245994ae8a2f00ea11e007b77cede655bbf15d8686e2a3ed25f6e10c254be79ca',
@@ -34,6 +38,7 @@ const funder = Keypair.fromSecretKey(
 );
 
 interface Community {
+    name: string;
     pubkey: PublicKey;
     owner: PublicKey;
     primary: PublicKey;
@@ -41,6 +46,7 @@ interface Community {
 }
 
 interface Address {
+    name: string;
     key: Keypair;
     pubkey: PublicKey;
     assoc: PublicKey;
@@ -54,6 +60,7 @@ const staking = new Staking(
 const token = new Token(connection, mint, TOKEN_PROGRAM_ID, funder);
 
 async function createCommunity(
+    name: string,
     owner: PublicKey,
     primary: PublicKey,
     secondary?: PublicKey
@@ -72,50 +79,161 @@ async function createCommunity(
 
     await sendAndConfirmTransaction(connection, tx, [funder, comm]);
 
-    return { pubkey: comm.publicKey, owner, primary, secondary };
+    return { name, pubkey: comm.publicKey, owner, primary, secondary };
 }
 
-async function createFundedAddress(): Promise<Address> {
+async function createFundedAddress(name: string): Promise<Address> {
     const staker = new Keypair();
     let assoc = await token.getOrCreateAssociatedAccountInfo(staker.publicKey);
     await token.mintTo(assoc.address, mintAuthority, [], 400_000);
 
-    return { key: staker, pubkey: staker.publicKey, assoc: assoc.address };
+    return {
+        name,
+        key: staker,
+        pubkey: staker.publicKey,
+        assoc: assoc.address
+    };
 }
 
-async function createAddress(): Promise<Address> {
+async function createAddress(name: string): Promise<Address> {
     const staker = new Keypair();
     let assoc = await token.getOrCreateAssociatedAccountInfo(staker.publicKey);
-    return { key: staker, pubkey: staker.publicKey, assoc: assoc.address };
+    return {
+        name,
+        key: staker,
+        pubkey: staker.publicKey,
+        assoc: assoc.address
+    };
+}
+
+async function stake(staker: Address, community: Community, amount: bigint) {
+    const tx = new Transaction().add(
+        await Instruction.Stake(
+            programId,
+            funder.publicKey,
+            staker.pubkey,
+            staker.assoc,
+            community.pubkey,
+            feeRecipient,
+            community.primary,
+            community.secondary || PublicKey.default,
+            amount
+        )
+    );
+    await sendAndConfirmTransaction(connection, tx, [funder, staker.key]);
+    console.log(`${staker.name} staked ${amount} ZEE with ${community.name}`);
+}
+
+async function printBeneficiary(rps: BN, address: Address) {
+    const bene = await staking.getBeneficiary(address.pubkey);
+    console.log(address.name);
+    console.log(`==============`);
+    console.log(`\t       staked = ${bene.staked.toNumber()}`);
+    console.log(
+        `\t  harvestable = ${bene.holding.add(bene.calculateReward(rps))}`
+    );
+    const balance = await connection.getTokenAccountBalance(address.assoc);
+    console.log(`\twallet balance = ${balance.value.uiAmount}`);
+    console.log();
 }
 
 (async () => {
-    const settings = await staking.getSettings();
-
-    const staked1Primary = await createAddress();
-    const staked1Secondary = await createAddress();
+    const staked1Primary = await createAddress('Community1 Primary');
+    const staked1Secondary = await createAddress('Community1 Secondary');
     const staked1Community = await createCommunity(
+        'Community1',
         staked1Primary.pubkey,
         staked1Primary.pubkey,
         staked1Secondary.pubkey
     );
 
-    const staked2Primary = await createAddress();
-    const staked2Secondary = await createAddress();
+    const staked2Primary = await createAddress('Community2 Primary');
+    const staked2Secondary = await createAddress('Community2 Secondary');
     const staked2Community = await createCommunity(
+        'Community2',
         staked2Primary.pubkey,
         staked2Primary.pubkey,
         staked2Secondary.pubkey
     );
 
-    const staker = await createFundedAddress();
-    const stakerCommunity = await createCommunity(staker.pubkey, staker.pubkey);
+    const staker = await createFundedAddress('Staker');
+    const stakerCommunity = await createCommunity(
+        'StakerCommunity',
+        staker.pubkey,
+        staker.pubkey
+    );
 
-    const outsideStaker1 = await createFundedAddress();
-    const outsideStaker2 = await createFundedAddress();
+    const outsideStaker1 = await createFundedAddress('Outside Staker 1');
+    const outsideStaker2 = await createFundedAddress('Outside Staker 2');
+
+    const tx = new Transaction().add(
+        await Instruction.InitializeStake(
+            programId,
+            funder.publicKey,
+            staker.pubkey,
+            staked1Community.pubkey,
+            mint
+        ),
+        await Instruction.InitializeStake(
+            programId,
+            funder.publicKey,
+            staker.pubkey,
+            staked2Community.pubkey,
+            mint
+        ),
+        await Instruction.InitializeStake(
+            programId,
+            funder.publicKey,
+            outsideStaker1.pubkey,
+            stakerCommunity.pubkey,
+            mint
+        ),
+        await Instruction.InitializeStake(
+            programId,
+            funder.publicKey,
+            outsideStaker2.pubkey,
+            stakerCommunity.pubkey,
+            mint
+        )
+    );
+    await sendAndConfirmTransaction(connection, tx, [
+        funder,
+        staker.key,
+        outsideStaker1.key,
+        outsideStaker2.key
+    ]);
+
+    const printState = async () => {
+        const settings = await staking.getSettings();
+        const rps = settings.calculateRewardPerShare(new Date());
+
+        await printBeneficiary(rps, staked1Primary);
+        await printBeneficiary(rps, staked1Secondary);
+        await printBeneficiary(rps, staked2Primary);
+        await printBeneficiary(rps, staked2Secondary);
+
+        await printBeneficiary(rps, staker);
+
+        await printBeneficiary(rps, outsideStaker1);
+        await printBeneficiary(rps, outsideStaker2);
+    };
+
+    await printState();
+
+    await stake(outsideStaker1, stakerCommunity, 50_000n);
+    await stake(staker, staked1Community, 75_000n);
+    await stake(staker, staked2Community, 100_000n);
+
+    await wait(2000);
+
+    await printState();
 })()
     .then(() => process.exit(0))
     .catch((e) => {
         console.error(e);
         process.exit(1);
     });
+
+function wait(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
